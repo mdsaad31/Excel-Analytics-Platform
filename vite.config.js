2001a0fa1b392923f54683d5bb844dbad2719865
mdsaad31 <mohammedsaad0462@gmail.com>
2025-07-07 13:30:10 +0530

    updated vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      external: [
+        'mongodb',
+        'mongoose',
+      ],
+    },
+  },
 })
